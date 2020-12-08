bfa10e72873bf2392d61bd46bd69f31676b7277b
Gia Dat Doan <giadatdoan@gmail.com>
2020-12-08 02:27:20 -0500

    exercise-2 halfway

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
+const clientsObj = require('./data/clients');
+const clients = clientsObj.clients;
+
 express()
   .use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -19,5 +22,16 @@ express()
   .use(express.urlencoded({ extended: false }))
 
   // endpoints
+  .get('/clients', (req, res) => {
+    // console.log(clientsObj.clients[0].id);
+    // console.log(clients);
+    res.json({ status: 200, data: clients});
+  })
+  .get('/clients/:id', (req, res) => {
+    const clientId = req.params.id;
+    let clientInfo = clients.filter((client) => client.id == clientId);
+
+    res.json({ status: 200, data: clientInfo});
+  })
 
   .listen(8000, () => console.log(`Listening on port 8000`));
